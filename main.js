38d7ea7a0c269de7dc701c5c6d4fa137c538883a
vinicius <vinicius.silva_01@outlook.com>
2024-07-18 23:00:29 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// Atribuindo variavéis
+const form = document.getElementById("form");
+const campoA = document.getElementById('campoA');
+const campoB = document.getElementById('campoB');
+const mensagemFalha = document.querySelector('.message-error');
+let funciona = false;
+
+console.log(campoA.value);
+function calculaCampo(){    // funcão serve para fazer a mesma COISA toda hora
+    return campoB.value > campoA.value;
+}
+
+form.addEventListener('submit', function(e)
+//nessa funcão o 'e' é de evento e é o parametro/objeto de evento ele guarda as informações do meu submit do meu form //
+{   
+    e.preventDefault(); // previne o formulário a fazer a sua função default que é recarregar a página
+    funciona = calculaCampo(campoB.value) // atribuindo o valor da variavel a uma variavel boleana usando a função calcula tempo para isso comparando se B é maior que A se sim vai ser true
+        if(funciona){
+            const mensagemSucesso = document.querySelector('#mensagem-sucesso');
+             //não esquecer de declarar variavéis antes
+            mensagemSucesso.style.display ='flex'; 
+            campoA.value = "";
+            campoB.value = "";
+        }
+        else{
+            campoA.classList.add('error');
+            
+        } 
+    
+})
+    campoA.addEventListener('keyup', function(e){
+        funciona = calculaCampo(e.target.value)
+        if(!funciona){
+            mensagemFalha.style.display = 'flex';
+
+        }
+        else{
+            mensagemFalha.style.display = '';
+            campoA.classList.remove('error');
+        }
+        })
