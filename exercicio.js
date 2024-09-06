f9dd2098a5d920d61cb21da142a8bfc9e581ae63
Vinícius Silva <vinicius.silva_01@outlook.com>
2024-09-06 14:58:36 -0300

    initial commit

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function somar(a, b) {
+    return a * b;
+}
+console.log(somar(2, 4));
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(saudacao('Vinícius'));
---
+++ exercicio.ts
@@ -0,0 +1,11 @@
+function somar(a:number, b:number){
+    return a * b;
+}
+
+console.log(somar(2,4));
+
+function saudacao(nome:string){
+    return `Olá ${nome}`;
+}
+
+console.log(saudacao('Vinícius'));
